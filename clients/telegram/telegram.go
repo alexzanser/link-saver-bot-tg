653ad89d115c9e81efc905c6c39db7e829aa4852
tolkachev <alexzanser@mail.ru>
2022-03-03 23:43:01 +0300

    vasya228

---
+++ clients/telegram/telegram.go
@@ -0,0 +1,59 @@
+package telegram
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"path"
+	"strconv"
+)
+
+type Client struct {
+	host string
+	basePath string
+	client http.Client
+}
+
+func New(host string, token string) Client {
+	 return Client{
+		 host:	host,
+		 basePath: newBasePath(token),
+		 client: http.Client{},
+	 }
+}
+
+func newBasePath(token string) string {
+	return "bot" + token
+}
+
+func (c *Client) Updates(offset int, limit int) ([]Update, error) {
+	q := url.Values{}
+
+	q.Add("offset", strconv.Itoa(offset))
+	q.Add("limit", strconv.Itoa(limit))
+}
+
+func (c *Client) doRequest(method string , query url.Values) ([]byte, error) {
+	const errMsg = "can't do request"
+	u := url.URL{
+		Scheme: "https",
+		Host: c.host,
+		Path: path.Join(c.basePath, method),
+	}
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+
+	if err != nil {
+		return nil, 
+	}
+
+	req.URL.RawQuery = query.Encode()
+
+	if err != nil {
+		return nil, fmt.Errorf("can't do request : %w", err)
+	}
+}
+
+func (c *Client) SendMessage() {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alexzanser/telegramBotGo.git
+
+go 1.13
