3419a8b56777a7d73d1e689f0342b6f86a7b0127
tolkachev <alexzanser@mail.ru>
2022-03-06 18:41:51 +0300

    vasya228

---
+++ events/telegram/commands.go
@@ -0,0 +1,14 @@
+package telegram
+
+import (
+	"strings" 
+	"log"
+)
+
+func (p *Processor) doCmd(text string, chatID int, username string) error {
+	text = strings.TrimSpace(text)
+
+	log.Printf("got new command '%s' from '%s'", text, username)
+
+	
+}
